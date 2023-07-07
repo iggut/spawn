0a906e0f4ad15a04226371787e047cdae271e92f
IgguT <igor.gutchin@gmail.com>
2023-07-07 09:46:44 -0400

    chrome laggy when resizing

--- hosts/gs66/nvidia.nix
+++ hosts/gs66/nvidia.nix
@@ -43,13 +43,11 @@
     TERMINAL = "kitty";
     NIXOS_OZONE_WL = "1";
     QT_QPA_PLATFORMTHEME = "gtk3";
-    QT_SCALE_FACTOR = "1";
-    #MOZ_ENABLE_WAYLAND = "1";
+    MOZ_ENABLE_WAYLAND = "1";
     SDL_VIDEODRIVER = "wayland";
     _JAVA_AWT_WM_NONREPARENTING = "1";
-    QT_QPA_PLATFORM = "wayland-egl";
+    #QT_QPA_PLATFORM = "wayland-egl";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     WLR_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card0";
     WLR_NO_HARDWARE_CURSORS = "1"; # if no cursor,uncomment this line  
     # GBM_BACKEND = "nvidia-drm";
--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -19,8 +19,7 @@
       ANKI_WAYLAND = "1";
       DIRENV_LOG_FORMAT = "";
       WLR_DRM_NO_ATOMIC = "1";
-      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-      #QT_QPA_PLATFORM = "wayland";
+      QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       QT_STYLE_OVERRIDE = "kvantum";
       MOZ_ENABLE_WAYLAND = "1";
